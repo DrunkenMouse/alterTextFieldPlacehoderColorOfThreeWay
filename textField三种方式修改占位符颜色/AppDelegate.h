8ab69e7c47f40be3f8e61076e74e72804eea4081
残月 <632342527@qq.com>
2016-07-13 20:52:37 +0800

    Initial Commit

---
+++ textField三种方式修改占位符颜色/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textField三种方式修改占位符颜色
+//
+//  Created by 王奥东 on 16/7/13.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textField三种方式修改占位符颜色/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textField三种方式修改占位符颜色
+//
+//  Created by 王奥东 on 16/7/13.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
